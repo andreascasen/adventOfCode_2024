8f3b1d293db0d721af58ccea7541672e7a3a7258
Andreas Casen <andreas.casenarg@gmail.com>
2024-12-02 22:04:29 +0100

    setting up node

---
+++ node/Day1/Day1.ts
@@ -0,0 +1,11 @@
+/**
+ * 1. Pair up the lowest numbers of each list, then the scond lowest, etc
+ * 2. Within each pair, figure out how far apart the two numbers are.
+ * 3. Add upp all of those distances
+ *
+ */
+
+export const day1 = (listA: number[], listB: number[]) => {
+	const exampleListA = [3, 4, 2, 1, 3, 3]
+	const exampleListB = [4, 3, 5, 3, 9, 3]
+}
---
+++ node/index.ts
@@ -0,0 +1,7 @@
+import { day1 } from "./Day1/Day1";
+
+const results = {
+  day1: day1(),
+};
+
+console.log("results => ", { results });
